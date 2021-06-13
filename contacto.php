79d8b908e5bf0bc62ba5b7bfdb7a2952ba8e6c13
0918108910-Alexis <alexis.zanchz@gmail.com>
2021-06-13 17:49:15 -0500

    Initial commit

---
+++ contacto.php
@@ -0,0 +1,128 @@
+
+
+<!DOCTYPE HTML>
+
+<html>
+	<head>
+		<title>Contacto</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+	</head>
+	<body class="is-preload">
+		<div id="page-wrapper">
+
+			<!-- Header -->
+				<header id="header">
+					
+					<nav id="nav">
+						<ul>
+							<li><a href="index.php">Inicio</a></li>
+							<li>
+								
+								<ul>
+									<li><a href="left-sidebar.html">Left Sidebar</a></li>
+									<li><a href="right-sidebar.html">Right Sidebar</a></li>
+									<li><a href="no-sidebar.html">No Sidebar</a></li>
+									<li>
+										<a href="#">Submenu</a>
+										<ul>
+											<li><a href="#">Option 1</a></li>
+											<li><a href="#">Option 2</a></li>
+											<li><a href="#">Option 3</a></li>
+											<li><a href="#">Option 4</a></li>
+										</ul>
+									</li>
+								</ul>
+							</li>
+							<li><a href="contacto.php">Contacto</a></li>
+						</ul>
+					</nav>
+				</header>
+				
+				
+
+			<!-- Main -->
+				<div id="main" class="wrapper style1">
+					<div class="container">
+						<header class="major">
+							<h2>Contacto</h2>
+							<p>Es un placer saludarte, te invito conozcas mis Redes Sociales</p>
+						</header>
+
+						
+						<section id="four" class="wrapper style1 special fade-up">
+					<div class="container">
+						
+						<div class="box alt">
+							<div class="row gtr-uniform">
+								<section class="col-4 col-6-medium col-12-xsmall">
+								<img src="https://img.icons8.com/dusk/96/000000/whatsapp.png"/>
+									<h3>919-130-0421</h3>
+									
+								</section>
+								<section class="col-4 col-6-medium col-12-xsmall">
+								<img src="https://img.icons8.com/dusk/96/000000/facebook.png"/>
+									<h3>Aleex Sanchez</h3>
+									
+								</section>
+								<section class="col-4 col-6-medium col-12-xsmall">
+								<img src="https://img.icons8.com/officel/96/000000/instagram-new.png"/>
+									<h3>aleex___</h3>
+									
+								</section>
+								<section class="col-4 col-6-medium col-12-xsmall">
+								<img src="https://img.icons8.com/dusk/96/000000/google-logo--v1.png"/>
+									<h3>Alexis Sanchez</h3>
+									
+								</section>
+								<section class="col-4 col-6-medium col-12-xsmall">
+								<img src="https://img.icons8.com/dusk/96/000000/youtube--v2.png"/>
+									<h3>radeOn</h3>
+									
+								</section>
+								<section class="col-4 col-6-medium col-12-xsmall">
+								<img src="https://img.icons8.com/dusk/96/000000/discord-logo.png"/>
+									<h3>AleexSS14</h3>
+									
+								</section>
+							</div>
+						</div>
+						<footer class="major">
+							
+						</footer>
+					</div>
+				</section>
+
+								
+			<!-- Footer -->
+				<footer id="footer">
+					<ul class="icons">
+						<li><a href="#" class="icon brands alt fa-twitter"><span class="label">Twitter</span></a></li>
+						<li><a href="#" class="icon brands alt fa-facebook-f"><span class="label">Facebook</span></a></li>
+						<li><a href="#" class="icon brands alt fa-linkedin-in"><span class="label">LinkedIn</span></a></li>
+						<li><a href="#" class="icon brands alt fa-instagram"><span class="label">Instagram</span></a></li>
+						<li><a href="#" class="icon solid alt fa-envelope"><span class="label">Email</span></a></li>
+					</ul>
+					<ul class="copyright">
+						<li>&copy; Derechos de Autor-radeOn02.</li><li>Alumno: <a href="">Alexis Sanchez</a></li>
+					</ul>
+				</footer>
+
+		</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/jquery.dropotron.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+			<script src="http://maps.google.com/maps/api/js?sensor=false&callback=iniciar">
+			</script>
+
+	</body>
+</html>
---
+++ index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE HTML>
+
+<html>
+	<head>
+		<title>Git Mundo</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+	</head>
+	<body class="is-preload landing">
+		<div id="page-wrapper">
+
+			<!-- Header -->
+				<header id="header">
+				
+					<nav id="nav">
+						<ul>
+							<li><a href="index.php">Inicio</a></li>
+							<li>
+								
+								<ul>
+									<li><a href="left-sidebar.html">Left Sidebar</a></li>
+									<li><a href="right-sidebar.html">Right Sidebar</a></li>
+									<li><a href="no-sidebar.html">No Sidebar</a></li>
+									<li>
+									
+										<a href="#">Submenu</a>
+										<ul>
+											<li><a href="#">Option 1</a></li>
+											<li><a href="#">Option 2</a></li>
+											<li><a href="#">Option 3</a></li>
+											<li><a href="#">Option 4</a></li>
+										</ul>
+									</li>
+								</ul>
+							</li>
+							<li><a href="contacto.php">Contacto</a></li>
+						</ul>
+					</nav>
+				</header>
+
+			<!-- Banner -->
+				<section id="banner">
+					<div class="content">
+						<header>
+						<h2>Saludos Cordiales</h2>
+							<h2>Desarrollo Web Integral</h2>
+							
+							<p>El desarrollo web integral es el uso de múltiples tecnicas</p>
+							 y tecnologías para crear sitios con buen posicionamiento y alto rendimiento
+							 <p>Para muchos, en alguna oportunidad, ya sea en el trabajo o por un tema personal</p>
+						</header>
+						<span class="image"><img src="images/pic00.jpg" alt="" /></span>
+					</div>
+					
+				</section>
+
+			
+				
+			
+
+		</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/jquery.dropotron.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
