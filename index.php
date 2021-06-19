bf85d2863e65fd61e154f762564dd3c3efea2c77
0918108910-Alexis <alexis.zanchz@gmail.com>
2021-06-19 16:12:34 -0500

    modified:   index.php

--- index.php
+++ index.php
@@ -10,58 +10,43 @@
 	</head>
 	<body class="is-preload landing">
 		<div id="page-wrapper">
-
+		
 			<!-- Header -->
 				<header id="header">
 				
 					<nav id="nav">
 						<ul>
 							<li><a href="index.php">Inicio</a></li>
-							<li>
+						
 								
-								<ul>
-									<li><a href="left-sidebar.html">Left Sidebar</a></li>
-									<li><a href="right-sidebar.html">Right Sidebar</a></li>
-									<li><a href="no-sidebar.html">No Sidebar</a></li>
-									<li>
-									
-										<a href="#">Submenu</a>
-										<ul>
-											<li><a href="#">Option 1</a></li>
-											<li><a href="#">Option 2</a></li>
-											<li><a href="#">Option 3</a></li>
-											<li><a href="#">Option 4</a></li>
-										</ul>
-									</li>
-								</ul>
 							</li>
 							<li><a href="contacto.php">Contacto</a></li>
 						</ul>
 					</nav>
 				</header>
-
+				
 			<!-- Banner -->
 				<section id="banner">
 					<div class="content">
+					
 						<header>
-						<h2>Saludos Cordiales</h2>
+						<Marquee>
+							Saludos
+						</Marquee>
 							<h2>Desarrollo Web Integral</h2>
 							
 							<p>El desarrollo web integral es el uso de múltiples tecnicas</p>
-							 y tecnologías para crear sitios con buen posicionamiento y alto rendimiento
-							 <p>Para muchos, en alguna oportunidad, ya sea en el trabajo o por un tema personal</p>
+							 <p>y tecnologías para crear sitios con buen posicionamiento y alto rendimiento</p>
+							 <p>para muchos, en alguna oportunidad, ya sea en el trabajo o personal</p>
+							 <p>en la materia de Dispositivos Inteligetes es muy interesante y tambien</p>
+							 <p>lo curioso es que podemos conectar nuestra pagina con una app </p> 
+							 <p>estas dos materias van de la mano y enverdad es muy chido aprender </p> 
 						</header>
 						<span class="image"><img src="images/pic00.jpg" alt="" /></span>
 					</div>
 					
 				</section>
-
-			
-				
-			
-
 		</div>
-
 		<!-- Scripts -->
 			<script src="assets/js/jquery.min.js"></script>
 			<script src="assets/js/jquery.scrolly.min.js"></script>
@@ -71,6 +56,5 @@
 			<script src="assets/js/breakpoints.min.js"></script>
 			<script src="assets/js/util.js"></script>
 			<script src="assets/js/main.js"></script>
-
 	</body>
 </html>
